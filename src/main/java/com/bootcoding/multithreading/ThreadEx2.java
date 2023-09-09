111a703a7491fa75135494886cb72d092f81ed79
pratu-coder <wadgurepratiksha25@gmail.com>
2023-09-09 22:01:38 +0530

    ThreadEx2

---
+++ src/main/java/com/bootcoding/multithreading/ThreadEx2.java
@@ -0,0 +1,22 @@
+package com.bootcoding.multithreading;
+
+import com.bootcoding.multithreading.mythread.AlphabetThread;
+import com.bootcoding.multithreading.mythread.NumberThread;
+import com.bootcoding.multithreading.mythread.SpecialSymbolThread;
+
+public class ThreadEx2 {
+
+    public static void main(String[] args) {
+            System.out.println("Hello");
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getPriority());
+
+            Thread t1 = new NumberThread();
+            t1.start();
+            Thread t2 = new AlphabetThread();
+            t2.start();
+            Thread t3 = new SpecialSymbolThread();
+            t3.start();
+        }
+    }
+
